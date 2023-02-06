29903c33536156cd95931327528345e464859a2e
TeakIvy <legocjones@gmail.com>
2023-02-05 20:09:23 -0500

    Projects

--- src/screens/MyWorkScreen/Project/index.tsx
+++ src/screens/MyWorkScreen/Project/index.tsx
@@ -15,6 +15,55 @@ function Project(props: any) {
 					className="project-image"
 				/>
 			</a>
+			<div className="project-info">
+				<h2 className="project-title">{props.title}</h2>
+				<div className="project-description">{props.children}</div>
+				<span className="project-links">
+					{props.links.map((link: any, i: number) => (
+						<span key={`s${i}`}>
+							<a
+								href={link.link}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="project-link"
+							>
+								{link.name}
+							</a>
+
+							{props.links.length - 1 !== i && (
+								<span className="link-bullet">•</span>
+							)}
+						</span>
+					))}
+				</span>
+
+				<span className="project-out-links">
+					{props.codeLink && (
+						<a
+							href={props.codeLink}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="project-link"
+						>
+							<BsCodeSlash className="project-link" />
+						</a>
+					)}
+					{props.projectLink && (
+						<span>
+							{" "}
+							<span className="link-separator"></span>
+							<a
+								href={props.projectLink}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="project-link"
+							>
+								<RiExternalLinkLine className="project-link" />
+							</a>
+						</span>
+					)}
+				</span>
+			</div>
 		</div>
 	);
 }
--- src/screens/MyWorkScreen/index.tsx
+++ src/screens/MyWorkScreen/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 import "./index.css";
-import LeftProjectView from "./LeftProjectView";
 import Project from "./Project";
-import RightProjectView from "./RightProjectView";
 
 function MyWorkScreen() {
 	return (
@@ -19,6 +17,10 @@ function MyWorkScreen() {
 							{
 								name: "VS Code",
 								link: "https://code.visualstudio.com/",
+							},
+							{
+								name: "GitHub",
+								link: "github.com/teakivy/DarkIvy",
 							},
 						]}
 						projectLink="https://marketplace.visualstudio.com/items?itemName=TeakIvy.darkivy"
@@ -37,98 +39,54 @@ function MyWorkScreen() {
 							.
 						</p>
 					</Project>
+					<Project
+						image={require("../../assets/img/projects/pai.png")}
+						title="Pixel Art Image"
+						links={[
+							{ name: "NodeJS", link: "https://nodejs.org/" },
+							{
+								name: "TypeScript",
+								link: "https://www.typescriptlang.org/",
+							},
+						]}
+						projectLink="https://www.npmjs.com/package/pai-converter"
+					>
+						<p>
+							A custom image format for pixel art. Consistantly
+							smaller file sizes than PNG.
+						</p>
+					</Project>
 
-					{/* <span className="project-display-one">
-						<LeftProjectView
-							image={require("../../assets/img/projects/darkivy.png")}
-							title="DarkIvy"
-							links={[
-								{
-									name: "VS Code",
-									link: "https://code.visualstudio.com/",
-								},
-							]}
-							projectLink="https://marketplace.visualstudio.com/items?itemName=TeakIvy.darkivy"
-							codeLink="https://github.com/teakivy/DarkIvy"
-						>
-							<p>
-								A simple VS Code purple theme avaliable on the{" "}
-								<a
-									href="https://marketplace.visualstudio.com/items?itemName=TeakIvy.darkivy"
-									target="_blank"
-									rel="noreferrer"
-									className="purple-text link hover-underline-animation"
-								>
-									Visual Studio Marketplace
-								</a>
-								.
-							</p>
-						</LeftProjectView>
-					</span>
-					<span className="project-display-two">
-						<RightProjectView
-							image={require("../../assets/img/projects/pai.png")}
-							title="Pixel Art Image"
-							links={[
-								{ name: "NodeJS", link: "https://nodejs.org/" },
-								{
-									name: "TypeScript",
-									link: "https://www.typescriptlang.org/",
-								},
-							]}
-							projectLink="https://www.npmjs.com/package/pai-converter"
-						>
-							<p>A custom image format for pixel art</p>
-						</RightProjectView>
-					</span>
-					<span className="project-display-three">
-						<LeftProjectView
-							image={require("../../assets/img/projects/teakstweaks.png")}
-							title={"Teak's Tweaks"}
-							links={[
-								{
-									name: "Spigot API",
-									link: "https://hub.spigotmc.org/javadocs/bukkit/",
-								},
-								{
-									name: "Maven",
-									link: "https://maven.apache.org/",
-								},
-								{ name: "Java", link: "https://www.java.com/" },
-							]}
-							projectLink="https://www.spigotmc.org/resources/teaks-tweaks-1-17-1-18.94021/"
-							codeLink="https://github.com/teakivy/teaks-tweaks"
-						>
-							<p>
-								The popular{" "}
-								<a
-									href="https://vanillatweaks.net/"
-									target="_blank"
-									rel="noreferrer"
-									className="purple-text link hover-underline-animation"
-								>
-									Vanilla Tweaks
-								</a>{" "}
-								Datapacks, in Minecraft Plugin form.
-							</p>
-						</LeftProjectView>
-					</span>
-					<span className="project-display-four">
-						<RightProjectView
-							image={require("../../assets/img/projects/pai.png")}
-							title="Pixel Art Image"
-							links={[
-								{ name: "NodeJS", link: "https://nodejs.org/" },
-								{
-									name: "TypeScript",
-									link: "https://www.typescriptlang.org/",
-								},
-							]}
-							projectLink="https://www.npmjs.com/package/pai-converter"
-						>
-							<p>A custom image format for pixel art</p>
-						</RightProjectView>
-					</span> */}
+					<Project
+						image={require("../../assets/img/projects/teakstweaks.png")}
+						title={"Teak's Tweaks"}
+						links={[
+							{
+								name: "Spigot API",
+								link: "https://hub.spigotmc.org/javadocs/bukkit/",
+							},
+							{
+								name: "Maven",
+								link: "https://maven.apache.org/",
+							},
+							{ name: "Java", link: "https://www.java.com/" },
+						]}
+						projectLink="https://www.spigotmc.org/resources/teaks-tweaks-1-17-1-18.94021/"
+						codeLink="https://github.com/teakivy/teaks-tweaks"
+					>
+						<p>
+							The popular{" "}
+							<a
+								href="https://vanillatweaks.net/"
+								target="_blank"
+								rel="noreferrer"
+								className="purple-text link hover-underline-animation"
+							>
+								Vanilla Tweaks
+							</a>{" "}
+							Datapacks, in Minecraft Plugin form.
+						</p>
+					</Project>
 				</span>
 			</div>
 		</div>
