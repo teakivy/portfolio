1bc85277e7e7734dcdfa389efeedf17eeda2c0c3
TeakIvy <legocjones@gmail.com>
2024-03-01 19:54:30 -0500

    Update styling and content in StarterScreen

--- src/pages/airesearch/screens/StarterScreen/index.tsx
+++ src/pages/airesearch/screens/StarterScreen/index.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import "./index.css";
+import './index.css';
 
 function StarterScreen() {
 	return (
-		<div className="starter-screen">
-			<div className="research-content">
-				<span className="research-title not-draggable">How Does the Integration of AI in Classrooms Affect Students and Educators?</span>
-				<span className="research-by-cj not-draggable">
+		<div className='starter-screen'>
+			<div className='research-content'>
+				<span className='research-title not-draggable'>
+					How Does the Integration of AI in Classrooms Affect Students and
+					Educators?
+				</span>
+				<span className='research-by-cj not-draggable'>
 					- A Research Study by Collin Jones
 				</span>
 			</div>
