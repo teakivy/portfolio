c7c3f07be6a772dac3e1811452d7715eba9b6bc3
teakivy <legocjones@gmail.com>
2024-11-24 21:01:31 -0500

    Update projects

--- src/pages/home/screens/FooterScreen/index.tsx
+++ src/pages/home/screens/FooterScreen/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import "./index.css";
+import './index.css';
 
 function FooterScreen() {
-	return (
-		<div className="footer-section">
-			<div className="footer-content">
-				Designed & Developed by Collin Jones
-			</div>
-		</div>
-	);
+    return (
+        <div className='footer-section'>
+            <div className='footer-content'>
+                Designed & Developed by Collin Jones
+            </div>
+        </div>
+    );
 }
 
 export default FooterScreen;
